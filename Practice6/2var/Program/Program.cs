5fa5210da48b054c4ca5c8a028990f3d56c55817
student <student@C251U38>
2020-10-09 15:33:45 +0300

    prog

---
+++ Practice6/2var/Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace Program
+{
+  class Person
+  {
+    string Name;
+    string Surname;
+    System.DateTime DateTime;
+
+    Person(string Name, string Surname, DateTime Datetime)
+    {
+      this.Name = Name;
+      this.Surname = Surname;
+      
+    }
+    Person()
+    {
+      Name = "pass";
+      Surname = "pass";
+      DateTime = DateTime.MinValue;
+    }
+
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     
+    }
+  }
+}
